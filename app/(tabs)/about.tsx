6d6324c6df2f53fb251eea1771ce98eba64e6825
jeongho <hyeon9782@gmail.com>
2024-11-21 23:12:17 +0900

    feat: router, tabs, screen 학습

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,9 +1,10 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Link } from "expo-router";
+import { Text, View, StyleSheet } from "react-native";
 
-export default function Index() {
+export default function AboutScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
+      <Text style={styles.text}>About screen</Text>
     </View>
   );
 }
@@ -12,8 +13,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#25292e",
-    alignItems: "center",
     justifyContent: "center",
+    alignItems: "center",
   },
   text: {
     color: "#fff",
