415aa062a70d914b62e9c13616ca1f631e4863ab
jeongho <hyeon9782@gmail.com>
2024-11-24 18:10:02 +0900

    feat: webview 앱과 웹 통신

--- app/(tabs)/webview.tsx
+++ app/(tabs)/webview.tsx
@@ -1,10 +1,27 @@
 import { WebView } from "react-native-webview";
 import Constants from "expo-constants";
-import { StyleSheet } from "react-native";
+import { Alert, StyleSheet } from "react-native";
+import { useRef } from "react";
 
 export default function WebviewScreen() {
+  const webviewRef = useRef(null);
+
+  const sendMessageToWeb = () => {
+    const message = "Hello from App!";
+    const script = `window.postMessage('${message}', '*');`;
+    webviewRef.current.injectJavaScript(script);
+  };
+  const handleMessage = (event) => {
+    const message = event.nativeEvent.data;
+    Alert.alert("Message from Web", message);
+  };
   return (
-    <WebView style={styles.container} source={{ uri: "https://expo.dev" }} />
+    <WebView
+      ref={webviewRef}
+      style={styles.container}
+      source={{ uri: "https://expo.dev" }}
+      onMessage={handleMessage}
+    />
   );
 }
 
